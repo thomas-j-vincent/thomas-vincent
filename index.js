be87d769b44df92f7facd1401ca7e6ecbe803cfd
thomas-j-vincent <thomasjvincent@outlook.com>
2026-09-10 22:37:45 +0100

    boxes fade in and out

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 //document.getElementById("a").innerText = "Menu2";
-const button = document.getElementById("b");
 document.getElementById("name2").innerText="Thomas Vincent";
-document.getElementById("name3").innerText="Quantitative anlayst";
-button.addEventListener('click', function() {
-    window.location.href = "index.html";
-});
+document.getElementById("name3").innerText="Quantitative analyst";
 
 console.log(5 + 6);
 
 const boxes = document.querySelectorAll(".flex");
-
 const boxHeight = 150; // box height under .flex in css
 const gap = 100;
 //const maxScroll = 500;
@@ -17,19 +12,36 @@ const gap = 100;
 const spacing = boxHeight + gap;
 
 let scrollPosition = 0;
+let specifier;
+
+//if (boxes.classList.contains("a")){
+//    specifier = "a";
+//} else if (boxes.classList.contains("b")){
+//    specifier = "b";
+//} else {specifier = "c";
+//}
 
 boxes.forEach(box => {
 
     box.addEventListener("mouseenter", function() {
         box.classList.add("hovered");
+        updateBoxes();
     });
     box.addEventListener("mouseleave", function() {
         box.classList.remove("hovered");
+        updateBoxes();
     });
+   // box.addEventListener("click", function(){
+     //       window.location.href = `${specifier}page.html`;
+       //     updateBoxes();
+    //})
 });
 
 function updateBoxes() {
     boxes.forEach((box, index) => {
+
+        const rect = box.getBoundingClientRect();
+        const fadeDistance = 200;
 
         const cycleHeight = boxes.length * spacing;
 
@@ -38,13 +50,18 @@ function updateBoxes() {
 
         if (position < 0) {
             box.style.color = `red`;
+            //box.style.opacity = 
+            //`0.5`;
             // this was meconsole.log(box, "touching");
         }
         else{
             box.style.color = `black`;
+           // box.style.opacity = 
+           // `1`;
         }
 
         let newPosition;
+        let opacity = 1
 
         if (position < -boxHeight) {
             newPosition = position + boxes.length * spacing;
@@ -54,6 +71,20 @@ function updateBoxes() {
             newPosition = position;
            // box.style.transform = `translateY(${position}px)`;
         }
+
+        if(newPosition < 0) {
+            opacity = Math.max(
+                0,(newPosition + boxHeight)/ boxHeight
+            );
+        }
+        const bottomPosition = position + boxHeight;
+        if (bottomPosition > window.innerHeight) {
+            opacity = Math.min(
+                opacity, Math.max(0, (window.innerHeight - position) / boxHeight)
+            );
+        }
+
+            box.style.opacity = opacity;
 
         if(box.classList.contains("hovered")) {
             box.style.transform = 
