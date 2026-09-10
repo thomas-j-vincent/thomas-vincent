81c81dcd22f5e1794ae14496f88da4e6f4c0484c
thomas-j-vincent <thomasjvincent@outlook.com>
2026-09-10 19:35:33 +0100

    boxes scroll correctly now :)

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const boxes = document.querySelectorAll(".flex");
 
 const boxHeight = 150; // box height under .flex in css
 const gap = 100;
-const maxScroll = 500;
+//const maxScroll = 500;
 
 const spacing = boxHeight + gap;
 
@@ -21,23 +21,26 @@ let scrollPosition = 0;
 function updateBoxes() {
     boxes.forEach((box, index) => {
 
-        const position2 = 
-        ((index * spacing - scrollPosition) % (boxes.length * spacing) + (boxes.length * spacing)) % (boxes.length * spacing);
-        if (position2 < 0) {
+        const cycleHeight = boxes.length * spacing;
+
+        const position = 
+        ((index * spacing - scrollPosition + boxHeight) % cycleHeight + cycleHeight) % cycleHeight - boxHeight;
+
+        if (position < 0) {
             box.style.color = `red`;
             // this was meconsole.log(box, "touching");
         }
         else{
             box.style.color = `black`;
         }
-        if (position2 < -boxHeight) {
-            const newPosition = position2 + boxes.length * spacing;
+        if (position < -boxHeight) {
+            const newPosition = position + boxes.length * spacing;
             box.style.transform = `translateY(${newPosition}px)`;
             // this was meconsole.log(box, "cycled");
         } else{
-            box.style.transform = `translateY(${position2}px)`;
+            box.style.transform = `translateY(${position}px)`;
         }
-        console.log("pos2", position2)
+        //console.log("pos2", position)
     });
 }
 
